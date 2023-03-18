bd35b2a6be9723bc01b344cd19a145b045150772
nnamditony <njokuanthony9@gmail.com>
2023-03-18 01:45:25 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A Program that prints a text output using puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
