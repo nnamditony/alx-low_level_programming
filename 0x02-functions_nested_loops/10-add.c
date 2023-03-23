bc2cd0486bac5babddc73269d0794cf3524af915
nnamditony <njokuanthony9@gmail.com>
2023-03-23 04:18:58 +0000

    9-times_table.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - A function that adds two integers and returns the result.
+ * @x: First number to be added.
+ * @y: second number to be added.
+ * Return: An integer as result.
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
