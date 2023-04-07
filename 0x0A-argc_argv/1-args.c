68b851c9d249b469d6162d218264c244d8838a17
nnamditony <njokuanthony9@gmail.com>
2023-04-07 15:04:16 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A program that prints its name
+ * @argc: The argument counter.
+ * @argv: The array of pointer to the arguments.
+ *
+ * Return: always 0 (success).
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
