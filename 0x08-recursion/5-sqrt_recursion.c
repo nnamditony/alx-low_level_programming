0a67b45dd05bf6b6c61edeacc4551f3958a9316c
nnamditony <njokuanthony9@gmail.com>
2023-06-09 04:09:47 +0000

    Prints natural square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _sqrt_recursion - A function that returns natural square root of a number
+ * @n: Number to print its square root.
+ * Return: returns square root or -1.
+ */
+int _sqrt_recursion(int n)
+{
+	int sqrt = 1;
+	while (sqrt *sqrt < n)
+		sqrt++;
+
+	if ((sqrt * sqrt) > n)
+	{
+		return (-1);
+	}
+	else if ((sqrt * sqrt) == n)
+	{
+		return (sqrt);
+	}
+	else
+		return (_sqrt_recursion(n + 1));
+}
