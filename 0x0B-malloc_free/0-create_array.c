27342c9be35ea89712792b59c55aef7ec750a552
nnamditony <njokuanthony9@gmail.com>
2023-06-13 21:03:18 +0000

    creates array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,9 @@ char *create_array(unsigned int size, char c)
 
 	ar = (char *)malloc(size * sizeof(char));
 	if (size == 0)
+	{
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		ar[i] = c;
