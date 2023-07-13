484d0b68a98105a63f21571878fa7a1ce39e739a
nnamditony <njokuanthony9@gmail.com>
2023-07-13 20:37:45 +0000

    copy content

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,8 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *creat_buf(char *file);
+void file_close(int fd);
+int main(int argc, char *argv[]);
 
 #endif
